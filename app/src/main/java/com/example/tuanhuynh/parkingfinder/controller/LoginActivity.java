3a472027c465f053111188d5f044edce445c63ea
tuanhuynh52 <tuanhuynh5290@gmail.com>
2015-12-05 17:42:16 -0800

    finished

--- app/src/main/java/com/example/tuanhuynh/parkingfinder/controller/LoginActivity.java
+++ app/src/main/java/com/example/tuanhuynh/parkingfinder/controller/LoginActivity.java
@@ -74,6 +74,7 @@ public class LoginActivity extends AppCompatActivity {
             public void onClick(View v) {
                 Intent intent = new Intent(LoginActivity.this, RegisterActivity.class);
                 startActivity(intent);
+                finish();
             }
         });
         /*
